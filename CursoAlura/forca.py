aee85d6aaa488bdbdf5da785cd7ff5303e900f7c
Gustavo Martins <gusluud@gmail.com>
2022-02-06 18:18:44 -0300

    Update a lot

--- CursoAlura/forca.py
+++ CursoAlura/forca.py
@@ -2,6 +2,16 @@ def jogar():
 	print("*******************")
 	print("JOGO DA FORCA")
 	print("*******************")
-
+	acertou = False
+	enforcou = False
+	palavra_secreta = "melancia"
+	while(not enforcou and not acertou):
+		escolha = input("Escolha uma letra:").strip().casefold() 		#remoção de espaços e passa para lowercase
+		if(escolha == "quit"):			# remover aqui depois
+			break
+		for letra in palavra_secreta:
+			if(letra == escolha):
+				print("Tem")
+				break
 if(__name__ == "__main__"):
 	jogar()
